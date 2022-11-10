41e05d3be8500851fc467887da518f7b03134308
Austin Cunningham <aucunnin@redhat.com>
2022-11-10 11:25:43 +0000

    mastodon link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,10 +24,10 @@
             <a href="https://medium.com/@auscunningham">
               <i class="big medium icon"></i>
             </a>
-            <a rel="me" href="https://mastodon.ie/@austincunningham">
-            </a>
             <a href="https://austin-cunningham.surge.sh/">
               <i class="big edit outline icon"></i>
+            </a>
+            <a rel="me" href="https://mastodon.ie/@austincunningham">.
             </a>
           </h5>
         </div>
